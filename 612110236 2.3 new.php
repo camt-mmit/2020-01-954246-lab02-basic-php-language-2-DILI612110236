31a75c5c77844b0398e47028780a41f8ee364484
DILI612110236 <68410177+DILI612110236@users.noreply.github.com>
2020-10-30 20:45:48 +0800

    Add files via upload

---
+++ 612110236 2.3 new.php
@@ -0,0 +1,12 @@
+<?php
+/*ID: 612110236
+Name: Di Li
+ */
+   printf("input size: ");
+   fscanf(STDIN,"%d",$n);
+   for($a>=0;$a<$n;$a++)
+     {for ($b>=0;$b<$n;$b++) 
+        {if ($b<=$a) printf("*");
+        else printf(" ");
+    }printf("\n");
+  }
---
+++ 612110236 2.4 new.php
@@ -0,0 +1,13 @@
+<?php
+/*ID: 612110236
+Name: Di Li
+ */
+  printf("input size: ");
+   fscanf(STDIN,"%d",$n);
+   for($a>=0;$a<$n;$a++)
+     {
+    for ($b=$n;$b>=0;$b--) 
+    {if ($b<=$a) printf("*");
+    else printf(" ");
+         }printf("\n");    
+    }
